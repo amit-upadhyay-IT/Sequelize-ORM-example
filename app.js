3a6f24ba87fe0fd55d465682ea9560272caf69d3
Amit Upadhyay <youarefreak1@gmail.com>
2017-07-05 16:49:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var Sequelize = require("sequelize");
+///var Sequal_auto = require("sequelize-auto");
 
-var Sequelize = require("sequelize");
 var connection = new Sequelize("Exponentia_Dev", "Exponentia", "Exponentia~", {
     host:"10.250.18.50",
     dialect: 'mssql'
 });
+
+/// var connection = new Sequal_auto("Exponentia_Dev", "Exponentia", "Exponentia~", {
+///     host:"10.250.18.50",
+///     dialect: 'mssql'
+/// });
 
 // make sure you have created the database using pg Admin III 
 // var sequelize = new Sequelize("postgres://huxutede:CqOxnhGdwEiEhtdmh7y54YgSG3ncnRq3@babar.elephantsql.com:5432/huxutede");
@@ -19,6 +24,10 @@ var TableVar = connection.define(table_name, {
 var sequelizeFile = require('./sequelizefile.js');
 
 connection.sync().then(function() {
-    sequelizeFile.delete_table_record(TableVar, 3);
-    //sequelizeFile.update_table_record(TableVar, record_id);
+    ///console.log(connection.tables);
+    // sequelizeFile.select_from_table(TableVar, 2);
+    //sequelizeFile.update_table_record(TableVar, 1);
+
+
+    console.log(sequelizeFile.transaction_querries(connection, TableVar));
 });
