5e8f48325b542420d10362d1a24480e8e0f22b1e
Amit Upadhyay <youarefreak1@gmail.com>
2017-07-07 15:47:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var Sequelize = require("sequelize");
+var ted = require("tedious");
 ///var Sequal_auto = require("sequelize-auto");
 
 var connection = new Sequelize("Exponentia_Dev", "Exponentia", "Exponentia~", {
     host:"10.250.18.50",
-    dialect: 'mssql'
+    dialect: 'tedious'
 });
 
 /// var connection = new Sequal_auto("Exponentia_Dev", "Exponentia", "Exponentia~", {
@@ -23,11 +24,27 @@ var TableVar = connection.define(table_name, {
 
 var sequelizeFile = require('./sequelizefile.js');
 
+function sample_calling_sp()
+{
+//     connection.query('CALL Insert_Amit();').spread(
+//     function (settingName1, settingName2, settingName3, users) {
+// });
+
+    connection.query('Insert_Amit @ID=\'02\', @NAME=\'Girdhar\', @COUNTRY=NULL')
+    .then(function(result) {
+        console.log('RESULT', result);
+    })
+    .error(function(err) {
+        console.log(err);
+    });
+}
+
 connection.sync().then(function() {
     ///console.log(connection.tables);
     // sequelizeFile.select_from_table(TableVar, 2);
     //sequelizeFile.update_table_record(TableVar, 1);
 
+    //console.log(sequelizeFile.transaction_querries(connection, TableVar));
 
-    console.log(sequelizeFile.transaction_querries(connection, TableVar));
+    sample_calling_sp();
 });
